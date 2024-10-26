df7e2448f34284ab8fde52f7f0f6d15d25845f22
Zalak Patel <zalupatel1298@gmail.com>
2024-10-26 13:54:16 +0100

    first commit

---
+++ src/test/java/testcases/HomePageTest.java
@@ -0,0 +1,12 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest extends BaseClass{
+	
+	@Test
+	public void TC03_someName() {
+		System.out.println("Inside TC03");
+	}
+
+}
